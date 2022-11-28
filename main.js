d30f013c867b8eb0cdab4b54433752a1b5d2c06c
Linvi <lwy1096@gmail.com>
2022-11-28 18:46:08 +0900

    전체적으로 꼬인 내용 수정

--- main.js
+++ main.js
@@ -27,8 +27,9 @@ forList.forEach((forL,index)=>forL.addEventListener('click',()=>{
 //이미지 리스트
 const imageWrap = document.querySelector('#imageRoll');
 const imageRolles = imageWrap.querySelectorAll('li');
-const imageLines = document.querySelectorAll('#imageLine>li');
+const imageButtons = document.querySelectorAll('#imageLine>li');
 const imageNum = imageRolles.length;
+console.log(imageButtons);
 //let rolling = setInterval(roll,3000);;
 let paused = false;
 let index=0;
@@ -42,34 +43,41 @@ function rollPage(){
             index=0;
         } 
         updateRolling();
+        console.log(index);
     },3000);
 };
 //이미지 클릭
 function stopAutoRolling(){
-
+    let i= index;
     clearInterval(rolling);
 };
 function movePageRollig(){
     if(!paused){
         rollPage();
     }
+
 }
 //변경 함수
 function updateRolling(){
     imageRolles.forEach((imageRoll,index)=>{
         imageRoll.classList.remove('on')
-        imageLines[index].classList.add('on');
+        imageButtons[index].classList.remove('on');
     });
+    imageButtons[index].classList.add('on');
+    imageRolles[index].classList.add('on');
 }
 //클릭이벤트
-imageLines.forEach((imageLine,index)=>imageLine.addEventListener('click',()=>{
+imageButtons.forEach((imageButton,i)=>imageLine.addEventListener('click',()=>{
     paused=true;
     stopAutoRolling();
-    updateRolling();
-    console.log(index);
+    imageRolles.forEach((imageRoll,i)=>{
+        imageRoll.classList.remove('on')
+        imageButtons[i].classList.remove('on');
+    });
+    imageButtons[i].classList.add('on');
+    imageRolles[i].classList.add('on');
     setTimeout(() => {
-        paused = false;
+        paused = false; 
         movePageRollig();
     }, 1000);
 }));
-
