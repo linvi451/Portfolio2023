77015b426e3d72fe7b04f22eec8366c68076d84f
Linvi <lwy1096@gmail.com>
2022-12-28 08:49:35 +0900

    DB 수정완료

--- js/product.js
+++ js/product.js
@@ -1,38 +1,52 @@
 import products from './productData.js'
 //리스트
 const productBox = document.querySelector('.productbox');
-const productList = productBox.querySelectorAll('li');
 products.map((product,index)=>{
     let i = index;
     let Plist = document.createElement('li');
     Plist.innerHTML=
     `
-        <div class="listBox">
-            <a href="#">
-                <div class="productImgBox">
-                    <img src=${product.src} alt=${product.alt}>
-                </div>
-                <div class="productTextBox">
-                    <p class="productTitle">${product.Pname}</p>
-                    <p class="productPrice">${product.price}</p>
-                </div>
-            </a>
-            <div class="iconList">
-                <div class="tagList">
-                    <span>NEW</span>
-                    <span>BEST</span>
-                </div>
-                <div class="shopList">
+    <div class="listBox">
+        <a href="#">
+            <div class="productImgBox">
+                <img src=${product.src} alt=${product.alt}>
+            </div>
+            <div class="productTextBox">
+                <p class="productTitle">${product.Pname}</p>
+                <p class="productPrice">${product.price}</p>
+            </div>
+        </a>
+        <div class="iconList">
+            <div class="tagList">
+                <span class="newTag">${product.new}</span>
+                <span class="typeTag">${product.type}</span>
+            </div>
+            <div class="shopList">
                 <span><i class="far fa-heart"></i></span>
                 <span><i class="fa-solid fa-cart-shopping"></i></span>
-                </div>
             </div>
         </div>
+    </div>
     `;
     productBox.appendChild(Plist);
 }
 );
-        
+const productNewTag = document.querySelectorAll('.newTag');
+productNewTag.forEach((productNT,index)=>{
+    if(productNT.innerHTML===""){
+        productNT.remove()
+    }
+})
+const productTypeTag = document.querySelectorAll('.typeTag');
+productTypeTag.forEach((productTT,index)=>{
+    if(productTT.innerHTML===""){
+        productTT.remove()
+    }
+})
+// if(productNewTag=""){
+//     productNewTag.remove();
+// }
+
 //클릭 후 아이콘 알림
 const cartIcones = document.querySelectorAll('.fa-cart-shopping');
 const favorates = document.querySelectorAll('.fa-heart');
--- js/productData.js
+++ js/productData.js
@@ -1,115 +1,86 @@
  let products = [
     {
         id:0,
-        price:'3800원',
+        price:'3,800원',
         Pname: '만월회 음료 원액 베이스 10종'+"<br>"+'(2개입/12개입)',
-        src: "img/photo2.jpg",
-        alt:"이미지0"
+        src: "img/product_1.jpg",
+        alt:"이미지0",
+        new: "NEW",
+        type:"BEST"
     },
     {
         id:1,
-        price:'3800원',
-        Pname: '만월회 음료 원액 베이스 12종'+"<br>"+'(2개입/12개입)',
-        src: "img/photo2.jpg",
-        alt:"이미지0"
+        price:'17,900원',
+        Pname: '만월회 음료 원액 베이스 11종'+"<br>"+'(500g/1kg)',
+        src: "img/product_2.jpg",
+        alt:"이미지0",
+        new: "NEW",
+        type:""
     },
     {
         id:2,
-        price:'3800원',
-        Pname: '만월회 음료 원액 베이스 1종'+"<br>"+ '(2개입/12개입)',
-        src: "img/photo2.jpg",
-        alt:"이미지0"
+        price:'판매가 비공개',
+        Pname: '[첫 구매 500원] 만월회 음료 원액 베이스'+"<br>"+ '',
+        src: "img/product_3.jpg",
+        alt:"이미지0",
+        new: "",
+        type:"주문폭주"
     },
     {
         id:3,
-        price:'3800원',
-        Pname: '만월회 음료 원액 베이스 1종'+"<br>"+ '(2개입/12개입)',
-        src: "img/photo2.jpg",
-        alt:"이미지0"
+        price:'21,000원',
+        Pname: '만월회 선물세트 ( 5종 / 10종)',
+        src: "img/product_4.jpg",
+        alt:"이미지0",
+        new: "NEW",
+        type:"주문폭주"
+
     },
     {
         id:4,
-        price:'3800원',
-        Pname: '만월회 음료 원액 베이스 1종'+"<br>"+ '(2개입/12개입)',
-        src: "img/photo2.jpg",
-        alt:"이미지0"
+        price:'3,800원',
+        Pname: '만월회 소매넣기',
+        src: "img/product_5.jpg",
+        alt:"이미지0",
+        new: "",
+        type:"선물추천"
     },
     {
         id:5,
-        price:'3800원',
-        Pname: '만월회 음료 원액 베이스 1종'+"<br>"+ '(2개입/12개입)',
-        src: "img/photo2.jpg",
-        alt:"이미지0"
+        price:'19,900원',
+        Pname: '리겔: 눈금 계량컵',
+        src: "img/product_6.png",
+        alt:"이미지0",
+        new: "",
+        type:""
     },
     {
         id:6,
-        price:'3800원',
-        Pname: '만월회 음료 원액 베이스 1종'+"<br>"+ '(2개입/12개입)',
-        src: "img/photo2.jpg",
-        alt:"이미지0"
+        price:'8,000원',
+        Pname: '카프: 우유곽 글라스',
+        src: "img/product_7.png",
+        alt:"이미지0",
+        new: "",
+        type:""
     },
     {
         id:7,
-        price:'3800원',
-        Pname: '만월회 음료 원액 베이스 1종'+"<br>"+ '(2개입/12개입)',
-        src: "img/photo2.jpg",
-        alt:"이미지0"
+        price:'2,500원',
+        Pname: '베가: 미니계량컵',
+        src: "img/product_8.png",
+        alt:"이미지0",
+        new: "",
+        type:""
     },
     {
         id:8,
-        price:'3800원',
-        Pname: '만월회 음료 원액 베이스 1종'+"<br>"+ '(2개입/12개입)',
-        src: "img/photo2.jpg",
-        alt:"이미지0"
+        price:'가격문의',
+        Pname: '만월회 단체주문',
+        src: "img/product_9.jpg",
+        alt:"이미지0",
+        new: "NEW",
+        type:"BEST"
     },
-    {
-        id:9,
-        price:'3800원',
-        Pname: '만월회 음료 원액 베이스 1종'+"<br>"+ '(2개입/12개입)',
-        src: "img/photo2.jpg",
-        alt:"이미지0"
-    },
-    {
-        id:10,
-        price:'3800원',
-        Pname: '만월회 음료 원액 베이스 1종'+"<br>"+ '(2개입/12개입)',
-        src: "img/photo2.jpg",
-        alt:"이미지0"
-    },
-    {
-        id:11,
-        price:'3800원',
-        Pname: '만월회 음료 원액 베이스 1종'+"<br>"+ '(2개입/12개입)',
-        src: "img/photo2.jpg",
-        alt:"이미지0"
-    },
-    {
-        id:12,
-        price:'3800원',
-        Pname: '만월회 음료 원액 베이스 1종'+"<br>"+ '(2개입/12개입)',
-        src: "img/photo2.jpg",
-        alt:"이미지0"
-    },
-    {
-        id:13,
-        price:'3800원',
-        Pname: '만월회 음료 원액 베이스 1종'+"<br>"+ '(2개입/12개입)',
-        src: "img/photo2.jpg",
-        alt:"이미지0"
-    },
-    {
-        id:14,
-        price:'3800원',
-        Pname: '만월회 음료 원액 베이스 1종'+"<br>"+ '(2개입/12개입)',
-        src: "img/photo2.jpg",
-        alt:"이미지0"
-    },
-    {
-        id:15,
-        price:'3800원',
-        Pname: '만월회 음료 원액 베이스 1종'+"<br>"+ '(2개입/12개입)',
-        src: "img/photo2.jpg",
-        alt:"이미지0"
-    },
+
 ]
 export default products;
